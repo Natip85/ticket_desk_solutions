aa2a3ef7d1d8bd748308eb4538c930fff1eac5a3
Nati Peretz <nati.peretz1985@gmail.com>
2023-04-07 14:53:52 +0300

    fixed local//4200 bug

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiService } from './core/api.service';
+import { AuthService } from './core/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,14 @@ import { ApiService } from './core/api.service';
 export class AppComponent {
   title = 'frontend';
 
-  constructor(private api: ApiService){}
+  constructor(private api: ApiService, private auth: AuthService){}
 
   // ngAfterViewInit(): void {
   //       this.api.redirectToHome();
   //   }
+
+  loggedIn(): boolean {
+        return this.auth.isLoggedIn();
+    }
 
 }
